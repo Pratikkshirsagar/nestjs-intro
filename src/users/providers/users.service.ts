0bb9054060335adcaabacde8faba7ee594df6590
pratikkshirsagar <meetpratiik@gmail.com>
2025-02-02 11:58:07 +0530

    forwardRef used in circular dependency

--- src/users/providers/users.service.ts
+++ src/users/providers/users.service.ts
@@ -1,13 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject, forwardRef } from '@nestjs/common';
 import { GetUsersParamDto } from '../dtos/get-user-param.dto';
+import { AuthService } from 'src/auth/providers/auth.service';
 
 @Injectable()
 export class UsersService {
+  constructor(
+    @Inject(forwardRef(() => AuthService))
+    private readonly authService: AuthService,
+  ) {}
   public findAll(
     getUserParamDto: GetUsersParamDto,
     limit: number,
     page: number,
   ) {
+    const isAuth = this.authService.isAuth();
+
+    if (!isAuth) {
+      return;
+    }
+
     return [
       {
         firstName: 'Pratik',
